70fb66e68506edefda18a3f8a88c9c00a0f5d541
ANAMILE PEREZ VALVERDE <Z391316@santander.com.mx>
2023-01-19 14:00:53 -0600

    New front

--- back/index.js
+++ back/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const {Client} = require('pg')
+const { Client } = require('pg')
 const bodyParser = require('body-parser')
 const { response } = require('express')
 const app = express()
@@ -26,8 +26,8 @@ app.post('/api/login', (req, res) => {
 	const { username, password } = req.body
 	const databaseclient = new Client(credentials)
 	databaseclient.connect()
-	databaseclient.query("SELECT id, username FROM roddo.users WHERE status = 1 and username = '"+username+"' AND password = '"+ password+"'")
-	.then(response => {
+	databaseclient.query("SELECT id, username FROM roddo.users WHERE status = 1 and username = '" + username + "' AND password = '" + password + "'")
+		.then(response => {
 			if (response.rowCount > 0) {
 				console.log('Usuario existe')
 				res.status(200).send({
@@ -40,11 +40,11 @@ app.post('/api/login', (req, res) => {
 				res.status(400).send('Usuario no existe')
 			}
 			databaseclient.end()
-	}).catch(err => {
-		console.log(err)
-		res.status(500).send(err)
-		databaseclient.end()
-	})
+		}).catch(err => {
+			console.log(err)
+			res.status(500).send(err)
+			databaseclient.end()
+		})
 })
 
 
@@ -52,16 +52,90 @@ app.get('/api/productos', (req, res) => {
 	const databaseclient = new Client(credentials)
 	databaseclient.connect()
 	databaseclient.query("SELECT * FROM roddo.real_state_list")
-	.then(response => {
+		.then(response => {
 			console.log('Consulta de productos')
 			res.status(200).send(response.rows)
 			databaseclient.end()
-	}).catch(err => {
-		console.log(err)
-		res.status(500).send(err)
-		databaseclient.end()
-	})
+		}).catch(err => {
+			console.log(err)
+			res.status(500).send(err)
+			databaseclient.end()
+		})
 })
 
 
-app.listen(4000, () => console.log('servidor api arriba'))
+app.post('/api/eliminar', (req, res) => {
+	const { id } = req.body
+	const databaseclient = new Client(credentials)
+	databaseclient.connect()
+	databaseclient.query("delete from roddo.real_state_list  WHERE id = " + id + " ")
+		.then(response => {
+			console.log('Producto Eliminado')
+			res.status(200).send({ "status": "success", "message": "Producto Eliminado" })
+			databaseclient.end()
+		}).catch(err => {
+			console.log(err)
+			res.status(500).send(err)
+			databaseclient.end()
+		})
+})
+
+app.post('/api/guardar', (req, res) => {
+	const { description, field, construction, address, contactphone, contactemail, bathrooms, bedrooms, parkinglots } = req.body
+	const databaseclient = new Client(credentials)
+	databaseclient.connect()
+	databaseclient.query("INSERT INTO roddo.real_state_list(description, field,construction,address,contactphone, contactemail, bathrooms, bedrooms, parkinglots, createdate,deletedate,status) "
+		+ "VALUES ('" + description + "', " + field + "," + construction + ",'" + address + "', '" + contactphone + "', '" + contactemail + "'," + bathrooms + "," + bedrooms + "," + parkinglots + ",CURRENT_TIMESTAMP,null,1)")
+		.then(response => {
+			console.log('Producto Insertado')
+			res.status(200).send({ "status": "success", "message": "Producto Insertado" })
+			databaseclient.end()
+		}).catch(err => {
+			console.log(err)
+			res.status(500).send(err)
+			databaseclient.end()
+		})
+})
+
+app.post('/api/editar', (req, res) => {
+	const { id, description, field, construction, address, contactphone, contactemail, bathrooms, bedrooms, parkinglots, status } = req.body
+	const databaseclient = new Client(credentials)
+	if (status = 0) {
+		databaseclient.connect()
+		databaseclient.query("UPDATE roddo.real_state_list "
+			+ " SET description='" + description + "', field=" + field + ", construction=" + construction + ", address='" + address + "', contactphone='" + contactphone + "', contactemail='" + contactemail
+			+ "', bathrooms=" + bathrooms + ", bedrooms=" + bedrooms + ", parkinglots=" + parkinglots + ", deletedate=CURRENT_TIMESTAMP, status=0 "
+			+ " WHERE id = " + id + " ")
+			.then(response => {
+				console.log('Producto Actualizado')
+				res.status(200).send({ "status": "success", "message": "Producto Actualizado" })
+				databaseclient.end()
+			}).catch(err => {
+				console.log(err)
+				res.status(500).send(err)
+				databaseclient.end()
+			})
+	} else {
+		databaseclient.connect()
+		databaseclient.query("UPDATE roddo.real_state_list "
+			+ " SET description='" + description + "', field=" + field + ", construction=" + construction + ", address='" + address + "', contactphone='" + contactphone + "', contactemail='" + contactemail
+			+ "', bathrooms=" + bathrooms + ", bedrooms=" + bedrooms + ", parkinglots=" + parkinglots + " "
+			+ " WHERE id = " + id + " ")
+			.then(response => {
+				console.log('Producto Actualizado')
+				res.status(200).send({ "status": "success", "message": "Producto Actualizado" })
+				databaseclient.end()
+			}).catch(err => {
+				console.log(err)
+				res.status(500).send(err)
+				databaseclient.end()
+			})
+	}
+})
+
+app.listen(4000, async () => {
+	console.log('servidor api arriba')
+	const ascified = await asciify('loading.png', { fit: 'box', width: 10, height: 10 })
+	console.log(ascified)
+	console.log(figlet.textSync('Exam APV Roddo v. 1.0.0'))
+	})
